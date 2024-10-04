e53fdaba486436ce84bae0b76cdbc2029960a14a
MrSeager <seager1993@gmail.com>
2024-10-04 13:10:35 +0300

    10/4/2024

--- src/components/PageNavBar.tsx
+++ src/components/PageNavBar.tsx
@@ -1,19 +1,27 @@
 import React, { FC } from 'react';
 //Bootstrap
-import { Container, Nav, Navbar, Image } from 'react-bootstrap';
+import { Container, Nav, Navbar, Image, Offcanvas } from 'react-bootstrap';
 //Images
 import ImgLogo from '../images/logo.svg';
 
 const PageNavBar: FC = () => {
     return (
-        <Navbar fixed="top" data-aos="fade-down" className='pt-5'>
+        <Navbar fixed="top" expand="md" data-aos="fade-down" className='pt-5'>
             <Container fluid className='mx-lg-5 mx-3'>
                 <Navbar.Brand><Image fluid src={ImgLogo} alt='logo' className='w-75' /></Navbar.Brand>
-                <Nav className='gap-5 justify-content-between text-center'>
-                    <Nav.Link className='text-white cs-link px-0 my-0 py-1'>Features</Nav.Link>
-                    <Nav.Link className='text-white cs-link px-0 my-0 py-1'>Team</Nav.Link>
-                    <Nav.Link className='text-white cs-link px-0 my-0 py-1'>Sign In</Nav.Link>
-                </Nav>
+                <Navbar.Toggle className='shadow-none bg-white' />
+                <Navbar.Offcanvas placement="end" className='cs-bg-darkblue-second text-white'>
+                    <Offcanvas.Header closeButton className='cs-btn-3'>
+                        <Offcanvas.Title>Menu</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body className='d-flex flex-row justify-content-md-end'>
+                        <Nav className='gap-md-5 gap-2 justify-content-md-between justify-content-start text-md-center text-start'>
+                            <Nav.Link className='text-white cs-link px-0 my-0 py-1'>Features</Nav.Link>
+                            <Nav.Link className='text-white cs-link px-0 my-0 py-1'>Team</Nav.Link>
+                            <Nav.Link className='text-white cs-link px-0 my-0 py-1'>Sign In</Nav.Link>
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
             </Container>
         </Navbar>
     );
